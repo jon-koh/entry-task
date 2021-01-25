0ea146f50a5d2914cd979a166d5e4e72e26bc2dc
jon <kohj@garena.com>
2021-01-25 09:43:02 +0800

    added streams page

---
+++ src/views/Streams.js
@@ -0,0 +1,25 @@
+// import StreamCard from "../components/StreamCard"
+// import "./Home.css"
+// import Carousel from "../components/Carousel"
+// import LiveStreams from "../components/LiveStreams"
+// import HotClips from "../components/HotClips"
+
+// function Home() {
+//     return (
+//     	<div class="content">
+//     		<Carousel />
+//     		<LiveStreams />
+//     		<HotClips />
+//     	</div>
+// 	);
+// }
+
+// export default Home;
+
+function Streams() {
+	return (
+		<h2>Streams</h2>
+	);
+}
+
+export default Streams;
