dedeb1efe90046e34429204fa91a93d99f476405
jon <kohj@garena.com>
2021-01-28 16:52:03 +0800

    YAY all working with infinite scrolling

--- src/views/Streams.js
+++ src/views/Streams.js
@@ -1,21 +1,40 @@
 import "./Views.css"
-import React from "react";
+import React, { useState } from "react";
 import StreamList from "../components/StreamList"
-import { useAsync } from 'react-async';
+import InfiniteScroll from "react-infinite-scroll-component";
 
-const GetData = async () => 
-	await fetch("https://test.connect.booyah.live/api/v3/streams?cursor=0&count=50&lang=en", {
-		method: 'GET'
-	})
-	.then(response => (response.ok ? response : Promise.reject(response)))
-	.then(response => response.json())
+
+
+const fetchData = async (cursor, count) => {
+	const res = await fetch(`https://test.connect.booyah.live/api/v3/streams?cursor=${cursor}&count=${count}&lang=en`);
+	const response = await res.json();
+	return response;
+};
 
 function Streams() {
-  	const { data, error, isLoading } = useAsync({ promiseFn: GetData });
-	if (isLoading) return "wait";
-	if (error) return `died ${error.message}`;
-	if (data) {
-		return (
+   	const [streamList, setStreamList] = useState([]);
+	const [cursor, setCursor] = useState(0);
+	const [hasMore, setHasMore] = useState(true);
+
+    const loadData = (cursor, count) => {
+    	fetchData(cursor, count).then((response) => {
+			setCursor(response.cursor); 
+			setHasMore(response.cursor !== 0); 
+			setStreamList(streamList.concat(response.stream_list))
+		})
+    }
+
+    React.useEffect(() => {
+		loadData(0, 10)
+	}, []);
+
+	return (
+		<InfiniteScroll
+		dataLength={streamList.length}
+		next={() => loadData(cursor, 10)}
+		hasMore={hasMore}
+		loader={<div class="title">wait pls</div>}
+		>
 			<div class="stream-list section">
 				<div class="header">
 					<input type="text" placeholder="Search for a stream"/>
@@ -25,16 +44,16 @@ function Streams() {
 				</div>
 				<div class="header">
 					<span class="title">
-			            <b>
-			                Live Streams
-			            </b>
-			        </span>
+						<b>
+							Live Streams
+						</b>
+					</span>
 				</div>
-				<StreamList list={data.stream_list}/>
 			</div>
-		);
-	}
-	return null;
+			<StreamList list={streamList}/>
+		</InfiniteScroll>
+	);
 }
+
 
 export default Streams;
